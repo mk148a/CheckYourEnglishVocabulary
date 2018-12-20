85a29713dabb90b7ae9f55bb07e10cbdeb24cf7f
MURAT KANDİL <Mk148a@hotmail.com>
2018-12-20 19:14:50 +0300

    pattern and design change- still continue
    Signed-off-by: MURAT KANDİL <Mk148a@hotmail.com>

--- app/src/main/java/com/englishapps/com/checkyourenglishvocabulary/MainActivity.java
+++ app/src/main/java/com/englishapps/com/checkyourenglishvocabulary/MainActivity.java
@@ -12,13 +12,9 @@ import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,7 +25,6 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -167,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
                 // Establish the connection.
 
 
-                EditText usertxt = (EditText) findViewById(R.id.usernametxt);
+                TextView usertxt = (TextView) findViewById(R.id.usernametxt);
                 usertxt.setEnabled(true);
                 usertxt.setVisibility(View.VISIBLE);
                 usertxt.setText(kullaniciadi);
@@ -260,6 +255,25 @@ public class MainActivity extends AppCompatActivity {
 //            LinearLayout anasayfa = (LinearLayout) findViewById(R.id.anasayfa);
 //            orta.addView(ortaview());
 //            altlinear.addView(altview());
+            final Button singleplyr_btn = findViewById(R.id.btnSinglePlayer);
+            singleplyr_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent go_singlepage = new Intent();
+                    go_singlepage.setClass(MainActivity.this, singleplayer.class);
+                    startActivity(go_singlepage);
+                }
+            });
+            final Button multiplyr_btn = findViewById(R.id.btnMultiPlayer);
+            multiplyr_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent go_multiplyr = new Intent();
+                    go_multiplyr.setClass(MainActivity.this, Multiplayer.class);
+                    startActivity(go_multiplyr);
+                }
+            });
+
 
             if (giriscalisti == true) {
 //                ul.addView(ustviewuye());
@@ -301,87 +315,87 @@ public class MainActivity extends AppCompatActivity {
         return marg;
     }
 
-    public View ustview() throws IllegalAccessException {
-        ArrayList<String> adad = new ArrayList<String>();
-        adad.add("Login");
-        adad.add("Register");
-
-        int marg = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
-        int marg2 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 80, getResources().getDisplayMetrics());
-        LinearLayout linearLayout = new LinearLayout(this);
-        linearLayout.setOrientation(LinearLayout.VERTICAL);
-        LinearLayout.LayoutParams pr = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        pr.setMargins(marg2, (int) (marg * 0.50), 0, 0);
-        linearLayout.setLayoutParams(pr);
-
-        TableLayout tableLayout = new TableLayout(this);
-        tableLayout.setStretchAllColumns(false);
-        TableLayout.LayoutParams tb = (new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
-
-        tableLayout.setLayoutParams(tb);
-        tableLayout.setWeightSum(2);
-        int k = 1;
-        String[] value = null;
-        for (int i = 0; i < 1; i++) {
-            TableRow tableRow = new TableRow(this);
-            tableRow.setGravity(Gravity.NO_GRAVITY);
-            int marg1 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
-            TableLayout.LayoutParams prr = new TableLayout.LayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
-
-            tableRow.setLayoutParams(prr);
-
-            for (int j = 0; j < 2; j++) {
-                int bosluk = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3, getResources().getDisplayMetrics());
-                int genislik = (int) ((getResources().getDisplayMetrics().widthPixels / 3.5));
-                double oran = (float) 43 / (float) 110;
-                int yukseklik = (int) (oran * genislik);
-                //110x43 level buttonları
-                TableRow.LayoutParams tb1 = new TableRow.LayoutParams(genislik, yukseklik);
-                tb1.setMargins((int) (bosluk * 3.5), bosluk, bosluk, bosluk);
-                Button button = new Button(this);
-                //butonadı
-                button.setTag(adad.get(j));
-                button.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        //kanal adı ve linkini tagden alıyor
-                        String gelentag = v.getTag().toString();
-
-                        Intent i = new Intent();
-
-
-                        switch ((v.getTag().toString())) {
-                            case "Login":
-                                i.setClass(MainActivity.this, Login.class);
-                                break;
-                            case "Register":
-                                i.setClass(MainActivity.this, Register.class);
-                                break;
-
-                        }
-
-
-                        startActivity(i);
-                    }
-                });
-
-                if (j == 0) {
-                    button.setBackgroundResource(resimara(getString(R.string.loginbutton)));
-                } else if (j == 1) {
-                    button.setBackgroundResource(resimara(getString(R.string.registerbutton)));
-                }
-
-                tableRow.addView(button, tb1);
-                k = k + 1;
-            }
-
-            tableLayout.addView(tableRow);
-        }
-
-
-        linearLayout.addView(tableLayout);
-
-        return linearLayout;
-    }
+//    public View ustview() throws IllegalAccessException {
+//        ArrayList<String> adad = new ArrayList<String>();
+//        adad.add("Login");
+//        adad.add("Register");
+//
+//        int marg = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+//        int marg2 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 80, getResources().getDisplayMetrics());
+//        LinearLayout linearLayout = new LinearLayout(this);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//        LinearLayout.LayoutParams pr = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+//        pr.setMargins(marg2, (int) (marg * 0.50), 0, 0);
+//        linearLayout.setLayoutParams(pr);
+//
+//        TableLayout tableLayout = new TableLayout(this);
+//        tableLayout.setStretchAllColumns(false);
+//        TableLayout.LayoutParams tb = (new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT));
+//
+//        tableLayout.setLayoutParams(tb);
+//        tableLayout.setWeightSum(2);
+//        int k = 1;
+//        String[] value = null;
+//        for (int i = 0; i < 1; i++) {
+//            TableRow tableRow = new TableRow(this);
+//            tableRow.setGravity(Gravity.NO_GRAVITY);
+//            int marg1 = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, getResources().getDisplayMetrics());
+//            TableLayout.LayoutParams prr = new TableLayout.LayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT, 1.0f));
+//
+//            tableRow.setLayoutParams(prr);
+//
+//            for (int j = 0; j < 2; j++) {
+//                int bosluk = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3, getResources().getDisplayMetrics());
+//                int genislik = (int) ((getResources().getDisplayMetrics().widthPixels / 3.5));
+//                double oran = (float) 43 / (float) 110;
+//                int yukseklik = (int) (oran * genislik);
+//                //110x43 level buttonları
+//                TableRow.LayoutParams tb1 = new TableRow.LayoutParams(genislik, yukseklik);
+//                tb1.setMargins((int) (bosluk * 3.5), bosluk, bosluk, bosluk);
+//                Button button = new Button(this);
+//                //butonadı
+//                button.setTag(adad.get(j));
+//                button.setOnClickListener(new View.OnClickListener() {
+//                    public void onClick(View v) {
+//                        //kanal adı ve linkini tagden alıyor
+//                        String gelentag = v.getTag().toString();
+//
+//                        Intent i = new Intent();
+//
+//
+//                        switch ((v.getTag().toString())) {
+//                            case "Login":
+//                                i.setClass(MainActivity.this, Login.class);
+//                                break;
+//                            case "Register":
+//                                i.setClass(MainActivity.this, Register.class);
+//                                break;
+//
+//                        }
+//
+//
+//                        startActivity(i);
+//                    }
+//                });
+//
+//                if (j == 0) {
+//                    button.setBackgroundResource(resimara(getString(R.string.loginbutton)));
+//                } else if (j == 1) {
+//                    button.setBackgroundResource(resimara(getString(R.string.registerbutton)));
+//                }
+//
+//                tableRow.addView(button, tb1);
+//                k = k + 1;
+//            }
+//
+//            tableLayout.addView(tableRow);
+//        }
+//
+//
+//        linearLayout.addView(tableLayout);
+//
+//        return linearLayout;
+//    }
 
     private Intent rateIntentForUrl(String url) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.format("%s?id=%s", url, getPackageName())));
